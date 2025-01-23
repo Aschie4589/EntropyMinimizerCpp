2443fcd917e939262f6f8c053f0af560b3c47e55
Aschie4589 <tommaso.aschieri@gmail.com>
2025-01-23 16:41:05 +0100

    Finished first implementation of command line arguments. Working version. Missing save states and final output save.

--- src/main.cpp
+++ src/main.cpp
@@ -22,13 +22,12 @@
 
 int main(int argc, char** argv){
 
-    // get message handler for command line arguments
+    // Get general purpose message handler
     MessageHandler* message_handler = new MessageHandler();
-
-
     // Step 0: Parse command line arguments. Parser is created with new, so it must be deleted.
     argparse::ArgumentParser* parser = parse_arguments(argc, argv);
     int N, d;
+    
 
     // Option 1: kraus was called
     if (parser->is_subcommand_used("kraus")){
@@ -91,32 +90,99 @@ int main(int argc, char** argv){
             serializer.serialize("kraus", output, *kraus_operators, "Kraus operators for a random unitary channel", d, N);
             // print exit message
             message_handler->message("Kraus operators saved to " + output + ".");
-
+            delete kraus_operators;
+            return 0;
         }
 
-    }   else {
-        N = 200;
-        d = 10;
+    } 
+
+    // If condition to decide if any of "singleshot" or "multishot" was called
+    if (parser->is_subcommand_used("singleshot") || parser->is_subcommand_used("multishot")){
+        // get the selected parser
+        argparse::ArgumentParser* subparser;
+        if (parser->is_subcommand_used("singleshot")){
+            subparser = &parser->at<argparse::ArgumentParser>("singleshot");
+        } else {
+            subparser = &parser->at<argparse::ArgumentParser>("multishot");
+        }
+
+        // check printing and logging options and create logger or printer accordingly. Don't give file names or anything.
+        if (subparser->get<bool>("-l")){
+            message_handler->createLogger();
+        } 
+        if (!subparser->get<bool>("-s")){
+            message_handler->createPrinter();
+        }
+
+        // now also print to message handler
+        // first print the full command line
+        std::string full_command = "Command called: ";
+        for (int i = 0; i < argc; i++){
+            full_command += argv[i];
+            full_command += " ";
+        }
+        message_handler->message(full_command);
+        // Now explicitly print the options
+        message_handler->message("Parsed Kraus operators: " + subparser->get<std::string>("-k"));
+        // print logging and printing options,
+        message_handler->message("Logging is: " + std::to_string(subparser->get<bool>("-l")));
+        message_handler->message("Printing is: " + std::to_string(subparser->get<bool>("-s") ));
+
+        // Try to load kraus
+        std::vector<std::complex<double> >* kraus_operators = new std::vector<std::complex<double> >();
+        VectorSerializer serializer = VectorSerializer();
+        // get the kraus operators
+        DeserializedData deserialized_data = serializer.deserialize(subparser->get<std::string>("-k"));
+        kraus_operators = &deserialized_data.vectorData;
+        // print exit message
+        message_handler->message("Kraus operators loaded from " + subparser->get<std::string>("-k") + ".");
+        // get N and d from metadata
+        N = deserialized_data.N;
+        d = deserialized_data.d;
+        // log the N and d
+        message_handler->message("N: " + std::to_string(N));
+        message_handler->message("d: " + std::to_string(d));
+
+        // initialize configuration
+        EntropyConfig config = EntropyConfig();
+        // if present, set the number of iterations
+        if (subparser->is_used("-i")){
+            config.setMaxIterations(subparser->get<int>("-i"));
+        }
+        // if present, set the number of minimization attempts
+        if (parser->is_subcommand_used("multishot") && subparser->is_used("-a")){
+            config.setMinimizationAttempts(subparser->get<int>("-a"));
+        }
+        // set logging and printing
+        config.setLogging(subparser->get<bool>("-l"));
+        config.setPrinting(!subparser->get<bool>("-s"));
+
+        // finally, create a minimizer
+        EntropyMinimizer* minimizer = new EntropyMinimizer(kraus_operators, d, N, N, &config);
+         // Option 2: singleshot was called
+        if (parser->is_subcommand_used("singleshot")){
+            //log with message handler
+            message_handler->message("Subcommand singleshot was used");
+            // run single shot
+            minimizer->initializeRun();
+            minimizer->runMinimization();
+        }
+
+        // Option 3: multishot was called
+        if (parser->is_subcommand_used("multishot")){
+            //log with message handler
+            message_handler->message("Subcommand multishot was used");
+
+            // run multishot
+            minimizer->findMOE();
+        }
+        delete minimizer;
     }
-
-    // Option 2: singleshot was called
-    if (parser->is_subcommand_used("singleshot")){
-        std::cout << "Subcommand singleshot was used" << std::endl;
-    }
-
-    // Option 3: multishot was called
-    if (parser->is_subcommand_used("multishot")){
-        std::cout << "Subcommand multishot was used" << std::endl;
-    }
-
-
-    // Config setup
-    EntropyConfig config = EntropyConfig();
-
-
 
     // Delete the parser
     delete parser;
+    delete message_handler;
+    return 0;
 
 
     // Step 1: Generate the Kraus operators of the random unitary channel
@@ -132,19 +198,5 @@ int main(int argc, char** argv){
         delete new_haar_unitary;
     }
 
-
-    // Step 2: initialize the minimizer that will take care of finding the MOE
-    // Actual minimizer
-    EntropyMinimizer minimizer = EntropyMinimizer(kraus_operators, d, N, N, &config); 
-
-    // Step 3: run a minimization run
-    //minimizer.findMOE();
-
-    // Step 4: save final state
-    // minimizer.minimizer->saveState("final_state.dat");
-
-    // Step 5: free memory
-    delete kraus_operators;
-    return 0;
 }
 
